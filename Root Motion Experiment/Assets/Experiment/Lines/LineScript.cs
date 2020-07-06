5f1dff0273a9c914d44646d454e042b4daca8f72
RoundBearChoi <roundbeargames@gmail.com>
2020-07-06 15:01:59 +0900

    line renderer

---
+++ Root Motion Experiment/Assets/Experiment/Lines/LineScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roundbeargames
+{
+    public class LineScript : MonoBehaviour
+    {
+        LineRenderer lineRenderer;
+        [SerializeField] public GameObject StartObj;
+        [SerializeField] public GameObject CharacterHip;
+
+        private void Start()
+        {
+            lineRenderer = this.gameObject.GetComponent<LineRenderer>();
+        }
+
+        private void Update()
+        {
+            lineRenderer.SetPosition(0, StartObj.transform.position);
+            lineRenderer.SetPosition(1, CharacterHip.transform.position);
+        }
+    }
+}
